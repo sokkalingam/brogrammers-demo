18c2945a7a779b034e06fc91cdf972dbcadf8547
Sokkalingam Subramanian <sokkalingam.subramanian@zoominfo.com>
2020-05-02 19:18:30 -0400

    mongo-changes: config

---
+++ src/main/groovy/com/brogrammers/demo/config/WebConfig.groovy
@@ -0,0 +1,16 @@
+package com.brogrammers.demo.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+@EnableWebMvc
+class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+}
